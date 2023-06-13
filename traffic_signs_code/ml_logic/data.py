1429451e0c33753b34ee281d80ace599090fa082
Ludwig Matuschka <ludwigmatuschka@gmail.com>
2023-06-13 16:34:25 +0200

    data.py and params. py changed

--- traffic_signs_code/ml_logic/data.py
+++ traffic_signs_code/ml_logic/data.py
@@ -1,16 +1,22 @@
-import traffic_signs_code.params
+from traffic_signs_code.params import *
 import os
-import random
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+import cv2
+import numpy as np
 
-plt.figure(figsize=(20,20))
-img_folder=
-print(img_folder)
-for i in range(5):
-    file = random.choice(os.listdir(img_folder))
-    image_path= os.path.join(img_folder, file)
-    img=mpimg.imread(image_path)
-    ax=plt.subplot(1,5,i+1)
-    ax.title.set_text(file)
-    plt.imshow(img)
+def create_dataset():
+    img_folder =IMG_path
+    img_data_array=[]
+    class_name=[]
+
+    for dir1 in os.listdir(img_folder):
+        for file in os.listdir(os.path.join(img_folder, dir1)):
+            image_path= os.path.join(img_folder, dir1,  file)
+            image= cv2.imread( image_path, cv2.COLOR_BGR2RGB)
+            image=cv2.resize(image, (IMG_HEIGHT, IMG_WIDTH),interpolation = cv2.INTER_AREA)
+            image=np.array(image)
+            image = image.astype('float32')
+            image /= 255
+            img_data_array.append(image)
+            class_name.append(dir1)
+
+    return img_data_array, class_name
--- traffic_signs_code/params.py
+++ traffic_signs_code/params.py
@@ -1,7 +1,8 @@
 import os
 
-IMG_unrecognizable_path = os.path.join(os.getcwd(),'..','raw_data','Train','0')
+IMG_unrecognizable_path = os.path.join(os.getcwd(),'raw_data','Train','0')
 IMG_recognizable_path = os.path.join(os.getcwd(),'..','raw_data','Train','1')
+IMG_path =  os.path.join(os.getcwd(),'raw_data','Train')
 
 IMG_WIDTH = 100
-IMG_HIGHT = 100
+IMG_HEIGHT = 100
