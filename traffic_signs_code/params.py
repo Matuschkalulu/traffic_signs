a1e53749c2ae151b8c6b7fad69a2f7edb19f2e66
Mohamed Aly <mohamedscience7@gmail.com>
2023-06-18 13:36:35 +0200

    Added path to save croped images. Added path to load the Yolo model

--- traffic_signs_code/params.py
+++ traffic_signs_code/params.py
@@ -3,7 +3,8 @@ import os
 IMG_path =  os.path.join(os.getcwd(),'raw_data','Train')
 IMG_test_path = os.path.join(os.getcwd(),'raw_data','test_images')
 MODELS_path = os.path.join(os.getcwd(),'raw_data','models')
-
+Yolo_Model_path = os.path.join(os.getcwd(),'raw_data','models', 'yolo_v2.pt')
+Crop_path= os.path.join(os.getcwd(),'raw_data','Train', 'crop_images')
 
 IMG_WIDTH_BASE = 100
 IMG_HEIGHT_BASE = 100
