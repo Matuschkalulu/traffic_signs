73d253ddf11e54f8fbf9940e9669a7048b8c57af
Mohamed Aly <mohamedscience7@gmail.com>
2023-06-18 23:41:01 +0200

    Added labels parameter and new image width and height. Added train and test paths

--- traffic_signs_code/params.py
+++ traffic_signs_code/params.py
@@ -1,12 +1,19 @@
 import os
 
 IMG_path =  os.path.join(os.getcwd(),'raw_data','Train')
+Split_path =  os.path.join(os.getcwd(),'raw_data','split_data')
+Train_path =  os.path.join(os.getcwd(),'raw_data','split_data', 'train')
+Test_path =  os.path.join(os.getcwd(),'raw_data','split_data', 'test')
+
 IMG_test_path = os.path.join(os.getcwd(),'raw_data','test_images')
 MODELS_path = os.path.join(os.getcwd(),'raw_data','models')
 Yolo_Model_path = os.path.join(os.getcwd(),'raw_data','models', 'yolo_v2.pt')
-Crop_path= os.path.join(os.getcwd(),'raw_data','Train', 'crop_images')
+Crop_path= os.path.join(os.getcwd(),'raw_data', 'crop_images')
 
+labels = ['readable', 'unreadable']
 IMG_WIDTH_BASE = 100
 IMG_HEIGHT_BASE = 100
 IMG_WIDTH_VGG= 224
 IMG_HEIGHT_VGG = 224
+IMG_WIDTH_VGG_= 160
+IMG_HEIGHT_VGG_ = 160
