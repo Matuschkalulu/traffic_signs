223a308c83e546d41f502be7d0d57f43cf921366
Tidsanu Nampradid <Jtn_james@hotmail.com>
2023-06-18 16:57:52 +0100

    form_front_end

--- app.py
+++ app.py
@@ -6,7 +6,27 @@ from PIL import Image
 st.title("Traffic Sign Recognition")
 st.write("Identifie traffic signs in images.")
 
+st.sidebar.image("https://static.vecteezy.com/system/resources/previews/009/458/871/original/traffic-signs-icon-logo-design-template-vector.jpg", width=100)
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+# Loading CSS
+local_css("frontend/css/streamlit.css")
+
+
+def remote_css(url):
+    st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)
+
+# Loading CSS
+    local_css("frontend/css/streamlit.css")
+    remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
+
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+st.markdown('<style>...</style>', unsafe_allow_html=True)
+
 
 if uploaded_file is not None:
     # displaying the uploaded image
