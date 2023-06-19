16be10d6e6a5594aaca3d03847c2dd33f1cca4a3
Ludwig Matuschka <ludwigmatuschka@gmail.com>
2023-06-19 13:37:55 +0200

    Changes in Params

--- traffic_signs_code/params.py
+++ traffic_signs_code/params.py
@@ -1,7 +1,7 @@
 import os
 
-IMG_path =  os.path.join(os.getcwd(),'raw_data','Train')
-Split_path =  os.path.join(os.getcwd(),'raw_data','split_data')
+IMG_PATH =  os.path.join(os.getcwd(),'raw_data','Train')
+SPLIT_PATH =  os.path.join(os.getcwd(),'raw_data','split_data')
 Train_path =  os.path.join(os.getcwd(),'raw_data','split_data', 'train')
 Test_path =  os.path.join(os.getcwd(),'raw_data','split_data', 'test')
 
@@ -11,15 +11,8 @@ Yolo_Model_path = os.path.join(os.getcwd(),'raw_data','models', 'yolo_v2.pt')
 
 Crop_path= os.path.join(os.getcwd(),'raw_data', 'crop_images')
 
-VGG_Model_path = os.path.join(os.getcwd(),'raw_data','models', 'model.h5')
-Test_Image_path= os.path.join(os.getcwd(),'raw_data','test_images', '00007.jpg')
-Street_Image_path= os.path.join(os.getcwd(),'raw_data','street_images', '00001.jpg')
-Test_video_path= os.path.join(os.getcwd(),'raw_data','test_videos', 'video_1.mp4')
-Video_output_path= os.path.join(os.getcwd(),'raw_data','output_videos', 'output_video_1.mp4')
-Image_output_path= os.path.join(os.getcwd(),'raw_data','output_images', 'output_image.png')
+LABELS = ['readable', 'unreadable']
 
-
-labels = ['readable', 'unreadable']
 IMG_WIDTH_BASE = 100
 IMG_HEIGHT_BASE = 100
 IMG_WIDTH_VGG= 224
@@ -30,3 +23,12 @@ IMG_HEIGHT_VGG_ = 160
 
 LOCAL_MODEL_PATH= os.path.join(os.getcwd(),'models')
 
+
+
+#Can be deleted later
+VGG_Model_path = os.path.join(os.getcwd(),'raw_data','models', 'model.h5')
+Test_Image_path= os.path.join(os.getcwd(),'raw_data','test_images', '00007.jpg')
+Street_Image_path= os.path.join(os.getcwd(),'raw_data','street_images', '00001.jpg')
+Test_video_path= os.path.join(os.getcwd(),'raw_data','test_videos', 'video_1.mp4')
+Video_output_path= os.path.join(os.getcwd(),'raw_data','output_videos', 'output_video_1.mp4')
+Image_output_path= os.path.join(os.getcwd(),'raw_data','output_images', 'output_image.png')
