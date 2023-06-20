2b5aa9e68183c176fc8ecadf8b780681e31c6a7e
Tidsanu Nampradid <Jtn_james@hotmail.com>
2023-06-20 10:27:05 +0100

    video fixxing

--- pages/3_Video.py
+++ pages/3_Video.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import requests
-from PIL import Image
 
 # for the Streamlit interface
 st.title("Traffic Sign Recognition")
@@ -30,35 +29,28 @@ uploaded_file = st.file_uploader("Choose a video...", type=["mp4", "mov", "svi",
 st.markdown('<style>...</style>', unsafe_allow_html=True)
 
 if uploaded_file is not None:
-    if uploaded_file.type.startswith('video/'):
-        # displaying the uploaded video
-        st.header("Traffic sign in the video")
-        st.video(uploaded_file, caption="Uploaded Video", use_column_width=True)
 
-        # prediction
-        if st.button("Classify"):
-            # Prepare the video data
-            video_data = uploaded_file.read()
+    # displaying the uploaded video
+    st.header("Traffic sign in the video")
+    st.video(uploaded_file, caption="Uploaded Video", use_column_width=True)
 
-            # request for the API
-            recognition_url = "YOUR_API_ENDPOINT"
-            files = {"video": video_data}
-            response = requests.post(recognition_url, files=files)
+    # prediction
+    if st.button("Classify"):
+        # Prepare the video data
+        video_data = uploaded_file.read()
 
-            if response.status_code == 200:
-                # The prediction result
-                prediction = response.json()
-                traffic_sign = prediction["traffic_sign"]
-                confidence = prediction["confidence"]
+        # request for the API
+        recognition_url = "https://trafficsignscode-ugznwmrhlq-ew.a.run.app"
+        files = {"video": video_data}
+        response = requests.post(recognition_url, files=files)
 
-                st.success(f"Predicted traffic sign: {traffic_sign}")
-                st.info(f"Confidence: {confidence}")
-            else:
-                st.error("Failed to classify the video. Please try again.")
-    else:
-        # displaying the uploaded image
-        image = Image.open(uploaded_file)
-        st.header("Traffic sign in the image")
+        if response.status_code == 200:
+            # The prediction result
+            prediction = response.json()
+            traffic_sign = prediction["traffic_sign"]
+            confidence = prediction["confidence"]
 
-        with st.columns(3)[0]:
-            st.image(image, caption="Uploaded Image", use_column_width=True)
+            st.success(f"Predicted traffic sign: {traffic_sign}")
+            st.info(f"Confidence: {confidence}")
+        else:
+            st.error("Failed to classify the video. Please try again.")
