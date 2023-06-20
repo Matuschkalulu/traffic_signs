bbcfde4b02a9dc7ee56c4731239d9d0c059406d7
Mohamed Aly <mohamedscience7@gmail.com>
2023-06-20 20:10:40 +0200

    Added load_model function

--- traffic_signs_code/ml_logic/miscfunc.py
+++ traffic_signs_code/ml_logic/miscfunc.py
@@ -13,13 +13,13 @@ def load_model():
     """
 
     # Get the latest model version name by the timestamp on disk
-    local_model_directory = os.path.join(LOCAL_MODEL_PATH, 'first_model')
-    local_model_paths = glob.glob(f"{local_model_directory}/*")
-    if not local_model_paths:
-        return None
-    most_recent_model_path_on_disk = sorted(local_model_paths)[-1]
-    print(Fore.BLUE + f"\n{most_recent_model_path_on_disk}" + Style.RESET_ALL)
-    print(Fore.BLUE + f"\nLoad latest model from disk..." + Style.RESET_ALL)
-    latest_model = keras.models.load_model(most_recent_model_path_on_disk, compile=False)
+    local_model_directory = os.path.join(LOCAL_MODEL_PATH, 'improved_model3.h5')
+    #local_model_paths = glob.glob(f"{local_model_directory}/*")
+   # if not local_model_paths:
+   #     return None
+    #most_recent_model_path_on_disk = sorted(local_model_paths)[-1]
+    #print(Fore.BLUE + f"\n{most_recent_model_path_on_disk}" + Style.RESET_ALL)
+    #print(Fore.BLUE + f"\nLoad latest model from disk..." + Style.RESET_ALL)
+    latest_model = keras.models.load_model(local_model_directory, compile=False)
     print(":white_check_mark: Model loaded from local disk")
     return latest_model
