2e236741bdbf340d8f35107d2919bbd9b42fb90a
Mohamed Aly <mohamedscience7@gmail.com>
2023-06-21 13:20:32 +0200

    Adjusted the load_model function for the Resnet50

--- traffic_signs_code/ml_logic/miscfunc.py
+++ traffic_signs_code/ml_logic/miscfunc.py
@@ -2,8 +2,9 @@ import glob
 import os
 import time
 import pickle
+import tensorflow as tf
 from colorama import Fore, Style
-from tensorflow import keras
+from tensorflow.keras.optimizers import Adam
 from traffic_signs_code.params import *
 
 def load_model():
@@ -13,13 +14,17 @@ def load_model():
     """
 
     # Get the latest model version name by the timestamp on disk
-    local_model_directory = os.path.join(LOCAL_MODEL_PATH, 'improved_model3.h5')
+    local_model_directory = os.path.join(LOCAL_MODEL_PATH, 'improved_model_resnet_99.h5')
     #local_model_paths = glob.glob(f"{local_model_directory}/*")
    # if not local_model_paths:
    #     return None
     #most_recent_model_path_on_disk = sorted(local_model_paths)[-1]
     #print(Fore.BLUE + f"\n{most_recent_model_path_on_disk}" + Style.RESET_ALL)
     #print(Fore.BLUE + f"\nLoad latest model from disk..." + Style.RESET_ALL)
-    latest_model = keras.models.load_model(local_model_directory, compile=False)
+    latest_model = tf.keras.models.load_model(local_model_directory, compile=False)
+    latest_model.compile(loss='binary_crossentropy',
+                optimizer = Adam(learning_rate= 1e-4),
+                metrics=['accuracy'])
+
     print(":white_check_mark: Model loaded from local disk")
     return latest_model
