d5f42b3db2594e72c03095542c4ee2729e42467c
Mohamed Aly <mohamedscience7@gmail.com>
2023-06-22 23:27:47 +0200

    Added path to save the output image as figure and cleaned the code

--- traffic_signs_code/params.py
+++ traffic_signs_code/params.py
@@ -7,9 +7,7 @@ SPLIT_TEST_PATH =  os.path.join(os.getcwd(),'raw_data','split_data', 'test')
 
 IMG_TEST_PATH = os.path.join(os.getcwd(),'raw_data','test_images')
 MODELS_PATH = os.path.join(os.getcwd(),'raw_data','models')
-
 CROP_PATH= os.path.join(os.getcwd(),'raw_data', 'crop_images')
-
 LABELS = ['readable', 'unreadable']
 
 IMG_WIDTH_VGG= 224
@@ -19,9 +17,6 @@ IMG_HEIGHT_VGG_ = 160
 
 LOCAL_MODEL_PATH= os.path.join(os.getcwd(),'raw_data','models')
 
-
-
-#Can be deleted later
 VGG_Model_path = os.path.join(os.getcwd(),'raw_data','models', 'model.h5')
 Test_Image_path= os.path.join(os.getcwd(),'raw_data','test_images', '00007.jpg')
 Street_Image_path= os.path.join(os.getcwd(),'raw_data','street_images', '00001.jpg')
