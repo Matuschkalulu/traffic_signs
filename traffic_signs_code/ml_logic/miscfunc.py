3fb3c2ee8d395a041cc61f69207b0791aa0bfec1
Mohamed Aly <mohamedscience7@gmail.com>
2023-06-22 23:55:29 +0200

    Added the latest model path and removed the local model directory

--- traffic_signs_code/ml_logic/miscfunc.py
+++ traffic_signs_code/ml_logic/miscfunc.py
@@ -1,7 +1,5 @@
-import glob
 import os
 import time
-import pickle
 import tensorflow as tf
 from colorama import Fore, Style
 from tensorflow.keras.optimizers import Adam
@@ -12,11 +10,9 @@ def load_model():
     Return a saved model:
     Return None (but do not Raise) if no model is found
     """
-    local_model_directory = os.path.join(LOCAL_MODEL_PATH, 'first_model', 'improved_model_resnet_99.h5')
-    local_model_paths = glob.glob(f"{local_model_directory}/*")
-
+    latest_model_path = os.path.join(LOCAL_MODEL_PATH, 'improved_model_resnet_99.h5')
     print(Fore.BLUE + f"\nLoad latest model from disk..." + Style.RESET_ALL)
-    latest_model = tf.keras.models.load_model(local_model_directory, compile=False)
+    latest_model = tf.keras.models.load_model(latest_model_path, compile=False)
     latest_model.compile(loss='binary_crossentropy',
                 optimizer = Adam(learning_rate= 1e-4),
                 metrics=['accuracy'])
