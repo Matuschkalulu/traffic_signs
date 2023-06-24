4befed7d104aa571e71037d53445c0d440518fec
Mohamed Aly <mohamedscience7@gmail.com>
2023-06-24 16:14:48 +0200

    Added save model function

--- traffic_signs_code/ml_logic/miscfunc.py
+++ traffic_signs_code/ml_logic/miscfunc.py
@@ -1,20 +1,22 @@
-import os
-import time
-import tensorflow as tf
+from tensorflow import keras
 from colorama import Fore, Style
 from tensorflow.keras.optimizers import Adam
 from traffic_signs_code.params import *
 
-def load_model():
+def load_model(model_path: str) -> keras.Model:
     """
     Return a saved model:
-    Return None (but do not Raise) if no model is found
     """
-    latest_model_path = os.path.join(LOCAL_MODEL_PATH, 'improved_model_resnet_99.h5')
-    print(Fore.BLUE + f"\nLoad latest model from disk..." + Style.RESET_ALL)
-    latest_model = tf.keras.models.load_model(latest_model_path, compile=False)
+    latest_model = keras.models.load_model(model_path, compile=False)
     latest_model.compile(loss='binary_crossentropy',
                 optimizer = Adam(learning_rate= 1e-4),
                 metrics=['accuracy'])
-    print(":white_check_mark: Model loaded from local disk")
+    print('✅ Model is successfully loaded and compiled from local disk')
     return latest_model
+
+def save_model(model: keras.Model, model_path: str) -> None:
+    """
+    Saves the trained model to a local directory
+    """
+    model.save(model_path)
+    print('✅ Model is successfully saved to local disk')
